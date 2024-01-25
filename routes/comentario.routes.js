46b0f2bc66858c8bcaa48eb92d1938ab95ad32bb
Tomas <tomibudeguer20@gmail.com>
2024-01-25 17:33:25 -0300

    agregue foto de perfil en el modelo de comentario con validacion

--- routes/comentario.routes.js
+++ routes/comentario.routes.js
@@ -18,34 +18,35 @@ router
     [
       check("nombreUsuario")
         .notEmpty()
-        .withMessage("El nombre del usuario es un dato obligatorio")
-        .isLength({ min: 6 })
-        .withMessage(
-          "El nombre del producto debe contener como minimo 6 caracteres"
-        ),
+        .withMessage("El nombre del usuario es un dato obligatorio"),
+      // .isLength({ min: 6 })
+      // .withMessage(
+      //   "El nombre del producto debe contener como minimo 6 caracteres"
+      // )
+      check("fotoDePerfil")
+        .notEmpty()
+        .withMessage("La foto de perfil del usuario es un dato obligatorio"),
       check("comentario")
         .notEmpty()
         .withMessage("El comentario es un dato obligatorio")
-        .isLength({ min: 2, max: 200 })
-        .withMessage("El comentario debe contener entre 2 y 200"),
+        .isLength({ min: 10, max: 300 })
+        .withMessage("El comentario debe contener entre 10 y 300"),
       check("fecha")
         .notEmpty()
         .withMessage("La fecha es un dato obligatorio")
         .isLength({ min: 1 })
         .withMessage("La fecha debe contener como minimo 1 caracter"),
-      check("mostrar")
-        .notEmpty()
-        .withMessage("Mostrar es un dato obligatorio")
-        .isIn(["Si", "No"])
-        .withMessage("Mostrar debe ser correcta"),
+      // check("mostrar")
+      //   .notEmpty()
+      //   .withMessage("Mostrar es un dato obligatorio")
+      //   .isIn(["Si", "No"])
+      //   .withMessage("Mostrar debe ser correcta"),
       validarCampos,
     ],
     crearComentario
   );
 
-router
-.route("/mostrables")
-.get(listarComentariosMostrables);
+router.route("/mostrables").get(listarComentariosMostrables);
 
 router
   .route("/:id")
